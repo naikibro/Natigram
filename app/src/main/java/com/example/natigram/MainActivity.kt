fca1dbc679193e496c594dcc93e99cf77fab4054
naiki <naikibro@gmail.com>
2024-05-06 09:03:19 -1000

    feat(ActivityMain)
    - added button to redirect to login page
    - created intent to handle redirection
    
    TODO:
    - implement gradient background
    - implement login process
    - constraint the layouts

--- app/src/main/java/com/example/natigram/MainActivity.kt
+++ app/src/main/java/com/example/natigram/MainActivity.kt
@@ -1,22 +1,30 @@
 package com.example.natigram
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.natigram.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // Update the ID used here to match the ID of the ConstraintLayout in your XML layout
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.container)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
 
-
+    fun redirectToLogin(view: View) {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
