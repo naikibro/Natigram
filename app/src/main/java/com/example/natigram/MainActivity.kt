e160051d0dafa70efa614c43bc1291428c618481
naiki <naikibro@gmail.com>
2024-05-06 09:57:00 -1000

    feat(implemented gradient background)
    - added gradient background
    - resized to logo to match wireframe
    - used global variables for colors
    - secured the password entry
    - used constraint layout for login page
    
    TODO:
    - implement login process

--- app/src/main/java/com/example/natigram/MainActivity.kt
+++ app/src/main/java/com/example/natigram/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.natigram
 
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.View
-import androidx.activity.enableEdgeToEdge
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -12,14 +13,24 @@ import com.example.natigram.ui.login.LoginActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        // Update the ID used here to match the ID of the ConstraintLayout in your XML layout
+        
+        // Apply window insets for system bars
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.container)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Ensure the background of the LinearLayout is an AnimationDrawable
+        val linearLayout = findViewById<LinearLayout>(R.id.container)
+        val animationDrawable = linearLayout.background as? AnimationDrawable
+
+        animationDrawable?.apply {
+            setEnterFadeDuration(2500)
+            setExitFadeDuration(5000)
+            start()
         }
     }
 
